cbb71be5ac9172d752c9f05d9202f077bba152a8
José Atila Alvarado <j.alvarado@naomis.fr>
2025-03-19 11:38:57 +0100

    Mise à jour de la version du package à 0.1.1 et amélioration de la gestion des instances de contrôleur dans RouterRegistry

--- src/core/RouterRegistry.ts
+++ src/core/RouterRegistry.ts
@@ -1,19 +1,25 @@
 import { Application, RequestHandler } from "express"
 import "reflect-metadata"
-import { IRouter, IRouterConfig, MetadataKeys, RouterConfig } from "./MetadataStorage"
+import { IRouter, MetadataKeys, RouterConfig } from "./MetadataStorage"
+
+type ControllerClass = { new (): any }
 
 export class RouterRegistry {
-  static registerRoutes(app: Application, options: IRouterConfig = RouterConfig) {
+  static registerRoutes(app: Application, options: { apiPrefix?: string; controllers: ControllerClass[] }) {
     const apiPrefix = options.apiPrefix || RouterConfig.apiPrefix
+
     options.controllers.forEach(controller => {
+      const controllerInstance = new controller()
       const basePath = Reflect.getMetadata(MetadataKeys.BASE_PATH, controller)
       const routers: IRouter[] = Reflect.getMetadata(MetadataKeys.ROUTERS, controller) || []
       const controllerMiddlewares: RequestHandler[] = Reflect.getMetadata(MetadataKeys.MIDDLEWARES, controller) || []
+
       routers.forEach(route => {
         const { method, path, handlerName, middlewares: routeMiddlewares = [] } = route
-        const handler = controller.prototype[handlerName]
+        const handler = controllerInstance[handlerName].bind(controllerInstance)
         const allMiddlewares = [...controllerMiddlewares, ...routeMiddlewares]
         const fullPath = `${apiPrefix}${basePath}${path}`
+
         if (allMiddlewares.length > 0) {
           app[method](fullPath, ...allMiddlewares, handler)
         } else {
